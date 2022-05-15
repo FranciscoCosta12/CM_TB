2e3e96ab121a3b102c8659383195d68ea792ea1f
Francisco Costa <frcosta@ipvc.com>
2022-05-15 19:47:28 +0100

    Interface map

--- app/src/main/java/ipvc/estg/cm_tb/MainActivity.kt
+++ app/src/main/java/ipvc/estg/cm_tb/MainActivity.kt
@@ -1,11 +1,20 @@
 package ipvc.estg.cm_tb
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    fun btnOnClick(view: View) {
+        val intent = Intent(this, Map:: class.java)
+        startActivity(intent)
     }
 }
