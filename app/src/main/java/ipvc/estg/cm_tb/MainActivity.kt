4c3e8fae0a537c59a986e828010231c4aa0853f8
rduartesilva <79704100+rduartesilva@users.noreply.github.com>
2022-05-23 16:09:42 +0100

    Página de perfil terminada, feita por items.

--- app/src/main/java/ipvc/estg/cm_tb/MainActivity.kt
+++ app/src/main/java/ipvc/estg/cm_tb/MainActivity.kt
@@ -1,14 +1,19 @@
 package ipvc.estg.cm_tb
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import android.view.View
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
 
-        //teste no git
+    fun perfilClick(view: View) {
+        val intent = Intent (this, Perfil::class.java)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/ipvc/estg/cm_tb/Perfil.kt
@@ -0,0 +1,12 @@
+package ipvc.estg.cm_tb
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Perfil : AppCompatActivity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.perfil)
+
+        }
+}
