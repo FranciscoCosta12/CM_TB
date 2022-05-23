d8e710304cb9300b451cb6d7594a24e67ba09ba8
ipvcjdavid <jdavidmacedo@ipvc.pt>
2022-05-23 15:29:23 +0100

    v0.2 - sistemaReports - DropDownCompleto - Falta Multilíngua e Conexão FireBase

--- app/src/main/java/ipvc/estg/cm_tb/ReportActivity.kt
+++ app/src/main/java/ipvc/estg/cm_tb/ReportActivity.kt
@@ -3,9 +3,13 @@ package ipvc.estg.cm_tb
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.view.View
+import android.widget.*
 
 class ReportActivity : AppCompatActivity() {
+
+    lateinit var option : Spinner
+    lateinit var result : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,5 +20,16 @@ class ReportActivity : AppCompatActivity() {
             val IntentBackMain = Intent(this, MainActivity::class.java)
             startActivity(IntentBackMain)
         }
+
+        //Spinner Options ( opções de report )------------------------------------------------------
+        option = findViewById(R.id.spinner) as Spinner
+        result = findViewById(R.id.spinnerResult) as TextView
+
+        //Array de Opções
+        val options = arrayOf("Informação Falsa","Hacker Informático","Nome Inapropriado")
+
+        //addapter de opções ( Definir as opções no spinner )
+        option.adapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,options)
+
     }
 }
