bceb1fbb7bd3546ec7e9b392ee2573daea1534c7
rduartesilva <79704100+rduartesilva@users.noreply.github.com>
2022-05-24 12:27:02 +0100

    Página de perfil + menu.

--- app/src/main/java/ipvc/estg/cm_tb/MainActivity.kt
+++ app/src/main/java/ipvc/estg/cm_tb/MainActivity.kt
@@ -4,16 +4,28 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import ipvc.estg.cm_tb.databinding.ActivityMainBinding
+import ipvc.estg.cm_tb.databinding.ItemAddBinding
 
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-    fun perfilClick(view: View) {
-        val intent = Intent (this, Perfil::class.java)
+    fun btnPerfil(view: View) {
+        val intent = Intent(this, Perfil::class.java)
         startActivity(intent)
     }
+
+
+
+
 }
--- app/src/main/java/ipvc/estg/cm_tb/Perfil.kt
+++ app/src/main/java/ipvc/estg/cm_tb/Perfil.kt
@@ -1,12 +1,56 @@
 package ipvc.estg.cm_tb
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.util.Log
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.imageview.ShapeableImageView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.*
+import com.google.firebase.ktx.Firebase
+import java.lang.StringBuilder
 
 class Perfil : AppCompatActivity() {
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.perfil)
+
+    private  lateinit var firebaseAuth: FirebaseAuth
+
+    val dbref = FirebaseDatabase.getInstance().getReference("User")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.perfil)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+    }
+
+    /*
+    dbref.addValueEventListener(object : ValueEventListener{
+        override fun onDataChange(snapshot: DataSnapshot) {
+            if(snapshot.exists()){
+
+                for (userSnapshot in snapshot.children){
+                    val user = userSnapshot.getValue(UserModel::class.java)
+                    if(user?.userId == firebaseAuth.currentUser?.uid){
+                        val emailUser = user?.userEmail
+                        val nameUser = user?.userNome
+                        nome.text = nameUser.toString()
+                        email.text = emailUser.toString()
+                    }
+                }
+            }
 
         }
+
+        override fun onCancelled(error: DatabaseError) {}
+    })
+
+
+*/
+
+
 }
